d2fe2e8e1e3279df7d619b72b2792db289cf828b
Mark Johnson <mark.johnson@it.ox.ac.uk>
2014-01-24 16:30:35 +0000

    Lots of improvements to player page
    Made fonts bigger for controls, added button styling to some links,
    implemented mouseover scrolling for tab overflows

--- lfmc.user.js
+++ lfmc.user.js
@@ -13,8 +13,9 @@
 // @require http://yui.yahooapis.com/combo?3.14.1/yui-base/yui-base-min.js&3.14.1/oop/oop-min.js&3.14.1/event-custom-base/event-custom-base-min.js&3.14.1/features/features-min.js&3.14.1/dom-core/dom-core-min.js&3.14.1/dom-base/dom-base-min.js&3.14.1/selector-native/selector-native-min.js&3.14.1/selector/selector-min.js&3.14.1/node-core/node-core-min.js&3.14.1/color-base/color-base-min.js&3.14.1/dom-style/dom-style-min.js&3.14.1/node-base/node-base-min.js&3.14.1/event-base/event-base-min.js&3.14.1/event-delegate/event-delegate-min.js&3.14.1/node-event-delegate/node-event-delegate-min.js&3.14.1/pluginhost-base/pluginhost-base-min.js&3.14.1/pluginhost-config/pluginhost-config-min.js&3.14.1/node-pluginhost/node-pluginhost-min.js&3.14.1/dom-screen/dom-screen-min.js&3.14.1/node-screen/node-screen-min.js&3.14.1/node-style/node-style-min.js&3.14.1/attribute-core/attribute-core-min.js&3.14.1/event-custom-complex/event-custom-complex-min.js&3.14.1/attribute-observable/attribute-observable-min.js
 // @require http://yui.yahooapis.com/combo?3.14.1/attribute-extras/attribute-extras-min.js&3.14.1/attribute-base/attribute-base-min.js&3.14.1/attribute-complex/attribute-complex-min.js&3.14.1/base-core/base-core-min.js&3.14.1/base-observable/base-observable-min.js&3.14.1/base-base/base-base-min.js&3.14.1/base-pluginhost/base-pluginhost-min.js&3.14.1/classnamemanager/classnamemanager-min.js&3.14.1/event-synthetic/event-synthetic-min.js&3.14.1/event-focus/event-focus-min.js&3.14.1/widget-base/widget-base-min.js&3.14.1/widget-htmlparser/widget-htmlparser-min.js&3.14.1/widget-skin/widget-skin-min.js&3.14.1/widget-uievents/widget-uievents-min.js&3.14.1/arraylist/arraylist-min.js&3.14.1/base-build/base-build-min.js&3.14.1/widget-parent/widget-parent-min.js&3.14.1/widget-child/widget-child-min.js&3.14.1/tabview-base/tabview-base-min.js&3.14.1/plugin/plugin-min.js&3.14.1/event-simulate/event-simulate-min.js&3.14.1/async-queue/async-queue-min.js&3.14.1/gesture-simulate/gesture-simulate-min.js
 // @require http://yui.yahooapis.com/combo?3.14.1/node-event-simulate/node-event-simulate-min.js&3.14.1/event-key/event-key-min.js&3.14.1/node-focusmanager/node-focusmanager-min.js&3.14.1/tabview/tabview-min.js&3.14.1/array-extras/array-extras-min.js&3.14.1/history-base/history-base-min.js&3.14.1/yui-later/yui-later-min.js&3.14.1/history-html5/history-html5-min.js&3.14.1/history-hash/history-hash-min.js&3.14.1/history-hash-ie/history-hash-ie-min.js&3.14.1/router/router-min.js&3.14.1/pjax-base/pjax-base-min.js&3.14.1/querystring-stringify-simple/querystring-stringify-simple-min.js&3.14.1/io-base/io-base-min.js&3.14.1/pjax-content/pjax-content-min.js&3.14.1/pjax/pjax-min.js&3.14.1/querystring-parse/querystring-parse-min.js&3.14.1/querystring-stringify/querystring-stringify-min.js
-// @resource yuiskin http://yui.yahooapis.com/combo?3.14.1/widget-base/assets/skins/sam/widget-base.css&3.14.1/tabview/assets/skins/sam/tabview.css
+// @resource yuiskin http://yui.yahooapis.com/combo?3.14.1/widget-base/assets/skins/sam/widget-base.css&3.14.1/tabview/assets/skins/sam/tabview.css&3.14.1/cssbutton/cssbutton-min.css
 // ==/UserScript==
+
 
 YUI().use('node', 'tabview', 'querystring', 'pjax', function(Y) {
     var LFI = {
@@ -26,6 +27,8 @@ YUI().use('node', 'tabview', 'querystring', 'pjax', function(Y) {
         maincontent: {},
         glowsection: {},
         pjax: {},
+	scrolling: null,
+	scrollDiv: null,
 
         init: function() {
 	    this.login();
@@ -114,6 +117,14 @@ YUI().use('node', 'tabview', 'querystring', 'pjax', function(Y) {
 	    style += '.pagination {padding: .1em .2em;}';
 	    style += '#moreliketab .compact_info_snb {float:left;}';;
 	    style += '.playertab {height: 32em; overflow-y: scroll;overflow-x:hidden;}';
+	    style += '#product-summary a.remove-from-watchlist, #product-summary .yui3-button {font-size: 2em; line-height: 1em;}';
+	    style += '.title_children li a, .title_children li span, #moreeptab .left_col li, #moreeptab .left_col li .n_season {font-size: 1.5em;}';
+	    style += 'div.season div.list_episodes {width: 420px;}';
+	    style += '.scrollover {height: 2em; width: inherit; position: absolute; display: block;}';
+	    style += '.scrollover.up {top: 4.2em; cursor: n-resize;}';
+	    style += '.scrollover.down {top: 34.8em; cursor: s-resize;}';
+	    style += '.yui3-tab-panel > div {padding-top: 2em; padding-bottom: 2em;}';
+	    style += '.yui3-tab-label.yui3-tab-content {font-size: 2em; line-height: .7em}';
             GM_addStyle(style);
             Y.one('body').addClass('yui3-skin-sam');
         },
@@ -140,6 +151,7 @@ YUI().use('node', 'tabview', 'querystring', 'pjax', function(Y) {
             Y.one('.lf-main-navigation').remove(false);
             this.maincontent.removeClass('span-10');
             var watchlist = Y.one('a.watchlist-length');
+	    watchlist.addClass('yui3-button');
             watchlist.remove(false);
             Y.one('.search-bar').prepend(watchlist);
 	    backbutton =  this.body.create('<input type="button" id="backbutton" value="↩" />');
@@ -219,7 +231,7 @@ YUI().use('node', 'tabview', 'querystring', 'pjax', function(Y) {
 	    tabhtml += '<li><a href="#moreeptab">More Episodes</a></li>';
 	    tabhtml += '<li><a href="#moreliketab">More Like This</a></li>';
 	    tabhtml += '<li><a href="#reviewstab">Reviews</a></li>';
-	    tabhtml += '</ul><div id="#yui-tabcontents">';
+	    tabhtml += '</ul><div id="yui-tabcontents">';
 	    tabhtml += '<div id="infotab" class="playertab"></div>';
 	    tabhtml += '<div id="moreeptab" class="playertab"></div>';
 	    tabhtml += '<div id="moreliketab" class="playertab"></div>';
@@ -231,6 +243,12 @@ YUI().use('node', 'tabview', 'querystring', 'pjax', function(Y) {
 	    var moreep = Y.one('#main-content .section.season');
 	    var reviews = Y.one('#main_tabs');
 	    var morelike = Y.all('.page-related .section').pop();
+
+	    var syn = info.one('.synopsis');
+	    var syntext = this.body.create(syn.getHTML());
+	    syntext.addClass('lfmc-synopsis');
+	    info.insertBefore(syntext, syn);
+	    syn.remove();
 
 	    info.remove(false);
 	    tabcontainer.one('#infotab').append(info);
@@ -245,21 +263,66 @@ YUI().use('node', 'tabview', 'querystring', 'pjax', function(Y) {
 	    Y.one('.page-related').remove();
 
 	    morelikemask = morelike.one('.mask');
-	    morelike.all('.compact_info_snb').each(function(item) {
-		item.remove(false);
-		if (item.hasClass('first')) {
-		    item.removeClass('first');
-		}
-		morelikemask.append(item);
-	    });
-	    morelike.one('.wrapper').remove();
+	    morelikeinfos = morelike.all('.compact_info_snb');
+	    if (morelikeinfos.size()) {
+		morelikeinfos.each(function(item) {
+		    item.remove(false);
+		    if (item.hasClass('first')) {
+			item.removeClass('first');
+		    }
+		    morelikemask.append(item);
+		});
+		morelike.one('.wrapper').remove();
+	    }
+
+	    var scrollDown = this.body.create('<a class="scrollover down"></a>');
+	    scrollDown.on('mouseover', function(e) {
+		this.startScrollDown();
+	    }, this);
+	    scrollDown.on('mouseout', function(e) {
+		this.stopScroll();
+	    }, this);
+	    tabcontainer.append(scrollDown);
+
+	    var scrollUp = this.body.create('<a class="scrollover up"></a>');
+	    scrollUp.on('mouseover', function(e) {
+		this.startScrollUp();
+	    }, this);
+	    scrollUp.on('mouseout', function(e) {
+		this.stopScroll();
+	    }, this);
+	    tabcontainer.prepend(scrollUp);
+
+	    Y.all('#title-breadcrumb a').addClass('yui3-button');
 
 	    var tabview = new Y.TabView({
 		srcNode: '#yui-tabcontainer'
 	    });
 	    tabview.render();
-	
 
+	},
+
+	startScrollDown: function() {
+	    this.scrollDiv = Y.one('#yui-tabcontents .yui3-tab-panel-selected');
+	    this.scrolling = setInterval(function() {
+		if (LFI.scrollDiv) {
+		    LFI.scrollDiv.set("scrollTop", LFI.scrollDiv.get("scrollTop")+1);
+		}
+	    }, 10);
+	},
+
+	startScrollUp: function() {
+	    this.scrollDiv = Y.one('#yui-tabcontents .yui3-tab-panel-selected');
+	    this.scrolling = setInterval(function() {
+		if (LFI.scrollDiv) {
+		    LFI.scrollDiv.set("scrollTop", LFI.scrollDiv.get("scrollTop")-1);
+		}
+	    }, 10);
+	},
+
+	stopScroll: function() {
+	    clearInterval(this.scrolling);
+	    this.scrollDiv = null;
 	}
 
     }
@@ -267,3 +330,4 @@ YUI().use('node', 'tabview', 'querystring', 'pjax', function(Y) {
     LFI.init();
 
 });
+
